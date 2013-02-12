f8bcfa4273d57930fd1c6faa55d19a2a7374137e
Maia Grotepass <maiatoday@gmail.com>
2013-02-12 22:55:57 +0200

    LuckyDip comments

--- src/LuckyDip.h
+++ src/LuckyDip.h
@@ -8,6 +8,9 @@
 #ifndef LUCKYDIP_H_
 #define LUCKYDIP_H_
 
+/**
+ * As the LuckyDip for an image or a colour. It will give a time/weather appropriate image or color choosen from a pool.
+ */
 class LuckyDip {
 public:
 	LuckyDip();
