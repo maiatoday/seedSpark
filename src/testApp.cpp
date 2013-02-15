9c293c0a92c536b6493dab6a5f22fda7ac5dd324
Maia Grotepass <maiatoday@gmail.com>
2013-02-15 11:00:38 +0200

    Import kinect basic project

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -1,59 +1,172 @@
 #include "testApp.h"
 
 //--------------------------------------------------------------
-void testApp::setup(){
+void testApp::setup() {
+	setupKinect();
+	setupParticles();
 }
 
 //--------------------------------------------------------------
-void testApp::update(){
+void testApp::update() {
+	updateKinect();
+	updateParticles();
 }
 
 //--------------------------------------------------------------
-void testApp::draw(){
-
-}
-
-//--------------------------------------------------------------
-void testApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
-void testApp::keyReleased(int key){
+void testApp::draw() {
+	drawKinect();
+	drawParticles();
 
 }
 
 //--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
+void testApp::keyPressed(int key) {
 
 }
 
 //--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
+void testApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
+void testApp::mouseMoved(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
+void testApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
+void testApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
+void testApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
+void testApp::windowResized(int w, int h) {
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo) {
+
+}
+
+void testApp::setupKinect() {
+	isLive = true;
+	isTracking = false;
+	isTrackingHands = true;
+	isFiltering = false;
+	isCloud = false;
+	isCPBkgnd = true;
+	isMasking = true;
+
+	nearThreshold = 500;
+	farThreshold = 1000;
+
+	filterFactor = 0.1f;
+
+	ofBackground(0, 0, 0);
+
+	recordContext.setup();	// all nodes created by code -> NOT using the xml config file at all
+	//recordContext.setupUsingXMLFile();
+	recordDepth.setup(&recordContext);
+	recordImage.setup(&recordContext);
+
+	recordUser.setup(&recordContext);
+	recordUser.setSmoothing(filterFactor);				// built in openni skeleton smoothing...
+	recordUser.setUseMaskPixels(isMasking);
+	recordUser.setUseCloudPoints(isCloud);
+	recordUser.setMaxNumberOfUsers(MAX_NUMBER_USERS);// use this to set dynamic max number of users (NB: that a hard upper limit is defined by MAX_NUMBER_USERS in ofxUserGenerator)
+
+	recordHandTracker.setup(&recordContext, 4);
+	recordHandTracker.setSmoothing(filterFactor);		// built in openni hand track smoothing...
+	recordHandTracker.setFilterFactors(filterFactor);// custom smoothing/filtering (can also set per hand with setFilterFactor)...set them all to 0.1f to begin with
+
+	recordContext.toggleRegisterViewport();
+	recordContext.toggleMirror();
+
+}
+void testApp::updateKinect() {
+	// update all nodes
+	recordContext.update();
+	recordDepth.update();
+	recordImage.update();
+
+	// demo getting depth pixels directly from depth gen
+	depthRangeMask.setFromPixels(recordDepth.getDepthPixels(nearThreshold, farThreshold), recordDepth.getWidth(),
+			recordDepth.getHeight(), OF_IMAGE_GRAYSCALE);
+
+	// update tracking/recording nodes
+	if (isTracking)
+		recordUser.update();
+
+	// demo getting pixels from user gen
+	if (isTracking && isMasking) {
+		allUserMasks.setFromPixels(recordUser.getUserPixels(), recordUser.getWidth(), recordUser.getHeight(),
+				OF_IMAGE_GRAYSCALE);
+		user1Mask.setFromPixels(recordUser.getUserPixels(1), recordUser.getWidth(), recordUser.getHeight(),
+				OF_IMAGE_GRAYSCALE);
+		user2Mask.setFromPixels(recordUser.getUserPixels(2), recordUser.getWidth(), recordUser.getHeight(),
+				OF_IMAGE_GRAYSCALE);
+	}
+
+}
+void testApp::drawKinect() {
+	ofSetColor(255, 255, 255);
+
+	glPushMatrix();
+	glScalef(0.75, 0.75, 0.75);
+
+	if (isLive) {
+
+		recordDepth.draw(0, 0, 640, 480);
+		recordImage.draw(640, 0, 640, 480);
+
+		depthRangeMask.draw(0, 480, 320, 240);// can use this with openCV to make masks, find contours etc when not dealing with openNI 'User' like objects
+
+		if (isTracking) {
+			recordUser.draw();
+
+			if (isMasking)
+				drawMasks();
+
+		}
+		if (isTrackingHands)
+			recordHandTracker.drawHands();
+	}
+
+}
+void testApp::drawMasks() {
+	glPushMatrix();
+	glEnable (GL_BLEND);
+	glBlendFunc(GL_ONE_MINUS_DST_COLOR, GL_ZERO);
+	allUserMasks.draw(640, 0, 640, 480);
+	glDisable(GL_BLEND);
+	glPopMatrix();
+	user1Mask.draw(320, 480, 320, 240);
+	user2Mask.draw(640, 480, 320, 240);
+
+}
+
+void testApp::setupParticles() {
+
+}
+void testApp::updateParticles() {
+
+}
+void testApp::drawParticles() {
 
 }
--- src/testApp.h
+++ src/testApp.h
@@ -1,22 +1,53 @@
 #pragma once
-
+#include "ofxOpenNI.h"
 #include "ofMain.h"
+#include "LuckyDip.h"
 
-class testApp : public ofBaseApp{
+class testApp: public ofBaseApp {
 
-	public:
-		void setup();
-		void update();
-		void draw();
+public:
+	void setup();
+	void update();
+	void draw();
 
-		void keyPressed  (int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+
+	//------------------ kinect methods and variables
+	void setupKinect();
+	void updateKinect();
+	void drawKinect();
+	bool isLive, isTracking, isCloud, isCPBkgnd, isMasking;
+	bool isTrackingHands, isFiltering;
+
+	ofxOpenNIContext recordContext;
+	ofxDepthGenerator recordDepth;
+
+	ofxImageGenerator recordImage;
+
+	ofxHandGenerator recordHandTracker;
+
+	ofxUserGenerator recordUser;
+
+	void drawMasks();
+
+	int nearThreshold, farThreshold;
+
+	ofImage allUserMasks, user1Mask, user2Mask, depthRangeMask;
+
+	float filterFactor;
+
+	//------------------ particle methods and variables
+	LuckyDip lucky;
+	void setupParticles();
+	void updateParticles();
+	void drawParticles();
+
 };
